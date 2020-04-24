8666018d634729a24f68617d360ef84cbb3d8e4a
hujeery123 <925789661@qq.com>
2020-04-24 19:31:27 +0800

    【新增】创建微服务

---
+++ src/main/java/com/hjy/microfirst/MicroFirstApplication.java
@@ -0,0 +1,13 @@
+package com.hjy.microfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjy/microfirst/MicroFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjy.microfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
